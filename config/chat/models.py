de78be5629fed615f7d7a52ab367c4fb87dbef6b
nevadimka <myregbox@outlook.com>
2022-07-10 16:03:05 +0300

    Added models and media settings.

--- config/chat/models.py
+++ config/chat/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(
+        upload_to='user_avatars/', default='user_avatars/default.png', blank=True)
+
+    def __str__(self):
+        return f'{self.user.username}'
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        UserProfile, related_name='messages', on_delete=models.CASCADE)
+    text = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.sender.user.username
+
+
+class Room(models.Model):
+    participants = models.ManyToManyField(
+        UserProfile, related_name='rooms', blank=True)
+    messages = models.ManyToManyField(Message, blank=True)
+
+    def __str__(self):
+        return "{}".format(self.pk)
