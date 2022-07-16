f2629e620853c08c6766080589ffa60b915d41bb
nevadimka <myregbox@outlook.com>
2022-07-16 18:17:10 +0300

    Edited avatar image view.

--- config/config/urls.py
+++ config/config/urls.py
@@ -30,7 +30,6 @@ urlpatterns = [
     path('chat/', include('chat.urls')),
     path('sign/', include('sign.urls')),
 ]
-
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
